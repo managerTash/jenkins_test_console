32c6e41d24a56910ed2b8587dfacd4dafddc5efe
Emmanuel Owusu <eowusu@acsghana.com>
2019-07-21 22:11:51 +0000

    console feedback tested

---
+++ src/TEST_CONSOLE/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TEST_CONSOLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! And What is your name");
+            string username = Console.ReadLine();
+            Console.WriteLine("hello " + username + " how are you doing " + DateTime.Now.DayOfWeek);
+
+
+        }
+    }
+}
